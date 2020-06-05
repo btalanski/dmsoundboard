63b5ccaf01ad2402594662613bd46d93ab036251
Bruno Talanski <brn.talanski@gmail.com>
2020-06-04 21:30:37 -0300

    Initial server setup

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var merge = require("webpack-merge");
+
+const config = merge([{
+    entry: "./src/js/index.js",
+    output: {
+        filename: "app.[hash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        alias: {},
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Join session - DM Soundboard",
+            filename: "index.html",
+            template: "./src/index.html",
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            title: "Soundboard - DM Soundboard",
+            filename: "soundboard.html",
+            template: "./src/soundboard.html",
+            minify: false,
+        }),
+        new HtmlWebpackPlugin({
+            title: "Session - DM Soundboard",
+            filename: "session.html",
+            template: "./src/session.html",
+            minify: false,
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: "src/static/*",
+                to: "./static/[name].[ext]",
+                toType: "template",
+            }, ],
+        }),
+    ],
+}, ]);
+
+const devConfig = merge([{
+    module: {
+        rules: [{
+            test: /\.s[ac]ss$/i,
+            use: ["style-loader", "css-loader", "sass-loader"],
+        }, ],
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "./dist",
+    },
+}, ]);
+
+const prodConfig = merge([{
+    entry: "./src/js/index.js",
+    output: {
+        filename: "app.[hash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [{
+            test: /\.s[ac]ss$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        }, ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.[hash].css",
+        }),
+    ],
+}, ]);
+
+module.exports = () => {
+    const mode = process.env.NODE_ENV || "development";
+    console.log("Webpack mode: ", mode);
+    if (mode === "production") {
+        return merge(config, prodConfig, { mode });
+    }
+    return merge(config, devConfig, { mode });
+};
