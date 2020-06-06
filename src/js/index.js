8bb0d265b77ef511212eaabb128182ec1dcfec0b
Bruno Talanski <brn.talanski@gmail.com>
2020-06-06 13:17:02 -0300

    Add basic UI elements Add code to import audio file from user's file system Play sounds from user uploaded file

--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,61 @@ const updateRoomLink = (roomId) => {
     $roomLink.href = url;
 };
 
+const createAudioPlayer = () => {
+    const count = document.getElementsByClassName("audio-player");
+    const player = document.createElement("audio");
+    player.id = `audio-player-${count.length}`;
+    player.classList.add("audio-player");
+    player.controls = true;
+    player.loop = true;
+    return player;
+};
+
+const createSoundBoardItem = () => {
+    const count = document.getElementsByClassName("item-name");
+
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    const itemName = document.createElement("input");
+    itemName.value = `Audio ${count.length + 1}`;
+    item.id = `inputName[${count.length}]`;
+    item.name = `inputName[${count.length}]`;
+    itemName.classList.add("item-name");
+
+    item.append(itemName);
+
+    return item;
+};
+
+const addSoundBoardItem = (userFile) => {
+    console.log(userFile);
+    const $board = document.getElementById("soundboard");
+    const player = createAudioPlayer();
+    const $soundBoardItem = createSoundBoardItem();
+    $soundBoardItem.append(player);
+
+    const reader = new FileReader();
+    reader.onloadend = function(file) {
+        player.src = file.target.result;
+        player.play();
+    };
+    reader.readAsDataURL(userFile.files[0]);
+
+    $board.append($soundBoardItem);
+};
+
+const init = () => {
+    console.log("init app...");
+    const $audioInput = document.getElementById("audioInput");
+    $audioInput.addEventListener("change", function() {
+        addSoundBoardItem(this);
+    });
+};
+
 socket.on("connect", () => {
     console.log("connected to server");
     socket.emit("create-dm-room", {});
     socket.on("created-dm-room", (data) => updateRoomLink(data));
+    init();
 });
